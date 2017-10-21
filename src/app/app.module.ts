620252ece1ec5141ff95ce30c3fd89560475434b
Suwaphat <pond-2-pin@hotmail.com>
2017-10-21 11:51:03 +0700

    Angulas-Class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { FormsModule } from '@angular/forms'
+import { GetphotoService } from './services/getphoto.service'
+
+import { HttpModule } from '@angular/http'
+import {RouterModule,Routes} from "@angular/router" //เพิ่ม
+const appRoutes:Routes = [
+  {path:"",component:UserComponent},
+  {path:"about",component:AboutusComponent}
+] // เพิ่ม
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AboutusComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes) //เพิ่ม
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [AppComponent],
+  providers: [ GetphotoService ]
 })
 export class AppModule { }
