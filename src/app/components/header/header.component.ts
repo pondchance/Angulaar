b72aa50d8678d39b7b8d6d5743d46ec8f2913794
Suwaphat <pond-2-pin@hotmail.com>
2017-10-28 13:34:57 +0700

    add

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+private isLogin;
+
+  constructor(private loginService:LoginService,private router:Router) {
+
+  }
 
   ngOnInit() {
+  }
+
+  logOut(){
+	this.loginService.setUserLoggedOut();
+	this.router.navigate(['/']);
   }
 
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private username:string;
+  private password:string;
+  private result_text:string;
+
+  constructor(private loginService:LoginService,private router:Router) { }
 
   ngOnInit() {
+
+
+  }
+
+  login(username,password){
+
+    this.loginService.login(username,password).subscribe((response) => {
+      if(response.success == "true"){ //ถ้าเท่ากับทรูให้navigateไปหน้าhome
+          // this.result_text = "passed !";
+          this.loginService.setUserLoggedIn();
+          this.router.navigate(['home']);
+          console.log("Logging in ...");
+      }else{
+          this.result_text = "incorrect username or password!"; //ถ้าเป็นfalse
+      }
+    })
+    return false; //ไม่ให้หน้ารีเฟรช
   }
 
 }
+
